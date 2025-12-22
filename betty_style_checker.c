1993d0f3da90d89287dbe466ff256dc1775f6daf
root <alfaidi.afnan@gmail.com>
2025-12-22 19:29:51 +0000

    add task one: write code that passes the betty checks

---
+++ betty_style_checker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for betty style checker
+ *
+ * Description: This program prints "Hello, World"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, World\n");
+	return (0);
+}
